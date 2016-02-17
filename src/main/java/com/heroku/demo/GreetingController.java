b84de1505619d1919b98293606eb6beada5c483a
phyrionX <rgcmb@hotmail.com>
2016-02-17 12:46:17 +0100

    Subiendo la plantilla de Spring

---
+++ src/main/java/com/heroku/demo/GreetingController.java
@@ -0,0 +1,22 @@
+package com.heroku.demo;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello OP chetao, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value="/greetings/{name}", method = RequestMethod.GET)
+    public Greeting greeting(@PathVariable("name") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
